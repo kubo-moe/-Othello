3ec5258b7e87a4c90bf40f3fe8ae23f6ae3febf9
kubo-moe <mk710.149.555@gmail.com>
2020-07-29 11:17:48 +0900

    master

--- main.c
+++ main.c
@@ -3,22 +3,25 @@
 void syokika();							//関数宣言
 void masume(int tate, int yoko);		
 int nyuryoku(int& a, int& b);
-void return1(int a, int b, int tate, int yoko);
+int kuro(int a, int b, int tate, int yoko);
+int shiro(int a, int b, int tate, int yoko, char s);
 
-extern char s;
 
 int main(void){
-	int tate = 4;						//縦のマス目用
-	int yoko = 4;						//横のマス目用
+	int tate = 8;						//縦のマス目用
+	int yoko = 8;						//横のマス目用
 	int a = 0;							//行の入力用
 	int b = 0;							//列の入力用
 	int c = 0;
 	int d = 0;
+	char s[8][8] = {0};
 	syokika();
 	masume(tate, yoko);					//盤面の表示
 	nyuryoku(a, b);						//値の入力
-	return1(a, b, tate, yoko);			//石をひっくり返す
-	
+	kuro(a, b, tate, yoko);				//石をひっくり返す
+	nyuryoku(a, b);						//値の入力
+	shiro(a, b, tate, yoko, s[8][8]);	//石をひっくり返す
 	return 0;
 }
+
 
--- masume.c
+++ masume.c
@@ -3,12 +3,12 @@
 void masume(int tate, int yoko){
 	int i = 0;													//ループカウンタ
 	int j = 0;													//ループカウンタ
-	char s[4][4];
+	char s[8][8];
 	std::cout << "・リバーシを開始します。" << std::endl;
 	std::cout << "・あなたは黒石(○)です。" << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "  1 2 3 4" << std::endl;						//列を表示
+	std::cout << "  1 2 3 4 5 6 7 8" << std::endl;				//列を表示
      for(i = 0; i < tate; i++){                                 //縦に四つ表示するまでループ
     	std::cout << i + 1;                                     //行を表示
         for(j = 0; j < yoko; j++){                              //横に四つ表示するまでループ
