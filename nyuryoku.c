4fa1d2b166e29411758885c8e1c9ad7ce54af324
kubo-moe <mk710.149.555@gmail.com>
2020-08-20 13:23:35 +0900

    自動整形をかけました。

--- nyuryoku.c
+++ nyuryoku.c
@@ -1,26 +1,29 @@
-#include<iostream>
+#include <iostream>
 
-int nyuryoku(int& a, int& b){
-
-
-	do{
+int nyuryoku(int &a, int &b)
+{
+	do
+	{
 		std::cout << std::endl;
 		std::cout << "・置きたい行(縦)を入力してください：";
 		std::cin >> a;
 
-		if(a > 8 || a < 1){
-			std::cout << "・1~8の数字で入力して下さい。" <<std::endl;
+		if (a > 8 || a < 1)
+		{
+			std::cout << "・1~8の数字で入力して下さい。" << std::endl;
 		}
-	}while(a > 8 || a < 1);
+	} while (a > 8 || a < 1);
 
-	do{
+	do
+	{
 		std::cout << "・置きたい列(横)を入力してください：";
 		std::cin >> b;
 
-		if(b > 8 || b < 1){
-			std::cout << "・1~8の数字で入力して下さい。" <<std::endl;
+		if (b > 8 || b < 1)
+		{
+			std::cout << "・1~8の数字で入力して下さい。" << std::endl;
 		}
-	}while(b > 8 || b < 1);
+	} while (b > 8 || b < 1);
 
 	std::cout << std::endl;
 	return a, b;
