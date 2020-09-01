8e9a971e060281738b2db126b8f5c27ac9a2b17b
kubo-moe <mk710.149.555@gmail.com>
2020-09-01 17:26:44 +0900

    変数名を変更しました。

--- header.h
+++ header.h
@@ -1,9 +1,9 @@
-void initialization(int (&s)[8][8]);
-void board(int s[8][8]);
+void initialize(int (&s)[8][8]);
+void display(int s[8][8]);
 void input(int &a, int &b, int (&s)[8][8]);
 void reversi(int &a, int &b, int (&s)[8][8]);
-void updown(int a, int b, int white, int black, int sk, int counter,
-            int (&s)[8][8]);
-int diagonal(int a, int b, int white, int black, int sk, int counter,
+void decision(int a, int b, int white, int opponent, int sk, int counter,
+              int (&s)[8][8]);
+int diagonal(int a, int b, int white, int opponent, int sk, int counter,
              int (&s)[8][8]);
-void result(int (&s)[8][8]);
+void output(int (&s)[8][8]);
