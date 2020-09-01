ec2ae5a19c3b256f87fbc062d482ed6e3ac15dea
kubo-moe <mk710.149.555@gmail.com>
2020-09-01 17:27:50 +0900

    変数名を変更しました。

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 #include "header.h"
 
 int main(void) {
-  int s[8][8]{};      //オセロの石の格納用
-  initialization(s);  //配列の要素を初期化
-  board(s);           //盤面の表示
-  int a = 0;          //行の入力用
-  int b = 0;          //列の入力用
-  input(a, b, s);     //値の入力
-  result(s);          //結果を表示する
+  int s[8][8]{};  //オセロの石の格納用
+
+  initialize(s);   //配列の要素を初期化
+  display(s);      //盤面の表示
+  int a = 0;       //行の入力用
+  int b = 0;       //列の入力用
+  input(a, b, s);  //値の入力
+  output(s);       //結果を表示する
   return 0;
 }
