6047dc9ba6193440379b527c6606b4ed9af43a99
kubo-moe <mk710.149.555@gmail.com>
2020-09-02 15:53:56 +0900

    diagonal関数の型を変更しました。

--- header.h
+++ header.h
@@ -4,6 +4,6 @@ void input(int &a, int &b, int (&s)[8][8]);
 void reversi(int &a, int &b, int (&s)[8][8]);
 void decision(int a, int b, int white, int opponent, int sk, int counter,
               int (&s)[8][8]);
-int diagonal(int a, int b, int white, int opponent, int sk, int counter,
-             int (&s)[8][8]);
+void diagonal(int a, int b, int white, int opponent, int sk, int counter,
+              int (&s)[8][8]);
 void output(int (&s)[8][8]);
