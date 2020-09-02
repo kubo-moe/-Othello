cce22528e73b1a38346ac7c277ccba0a7b2174f2
kubo-moe <mk710.149.555@gmail.com>
2020-09-02 16:12:37 +0900

    do分の影響範囲を変更しました。

--- input.c
+++ input.c
@@ -6,19 +6,19 @@ void input(int &a, int &b, int (&s)[8][8]) {
   const char *character[30][30];  //日本語の文字列の格納用
   const char *stone[30][30];      //持石の文字列の格納用
   do {
+    if (counter == 0) {
+      character[30][30] = "あなた";
+      stone[30][30] = "白石(●)";
+    } else {
+      character[30][30] = "相手";
+      stone[30][30] = "黒石(○)";
+    }
+    std::cout << std::endl;
+    std::cout << num << "回目" << std::endl;  //回数表示
+    std::cout << "・" << character[30][30] << "の番です。" << stone[30][30]
+              << std::endl;  //テキストを表示;
+    std::cout << "・終了するときは9を入力してください。" << std::endl;
     do {
-      if (counter == 0) {
-        character[30][30] = "あなた";
-        stone[30][30] = "白石(●)";
-      } else {
-        character[30][30] = "相手";
-        stone[30][30] = "黒石(○)";
-      }
-      std::cout << std::endl;
-      std::cout << num << "回目" << std::endl;  //回数表示
-      std::cout << "・" << character[30][30] << "の番です。" << stone[30][30]
-                << std::endl;  //テキストを表示;
-      std::cout << "・終了するときは9を入力してください。" << std::endl;
       std::cout << "・置きたい行(縦)を入力してください：";
       std::cin >> a;
       if (a == 9) {
